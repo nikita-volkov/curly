687b6b2ec794e37479393e992383d91bfec2d8ee
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-08-20 15:36:47 +0300

    Sketch tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,11 +1,13 @@
 module Main where
 
+import qualified EchoHttpServer
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 import Prelude
 
 main =
-  defaultMain . testGroup "All"
-    =<< sequence
-      []
+  defaultMain . testGroup "All" $
+    [ testCase "" . EchoHttpServer.testWith $ \port ->
+        error "TODO"
+    ]
